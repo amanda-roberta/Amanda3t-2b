2cec960c8d468d28dfb9b8ecefc6c5516c111f83
amanda-roberta <amanda.pereira.dias@escola.pr.gov.br>
2024-11-21 16:27:32 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,40 @@
+criaCartao(
+    'Pessoal', 
+    'Qual a minha cor favorita?',
+    'verde'
+)
+
+criaCartao(
+    'Pessoal',
+    'Quais instrumentos eu sei tocar?',
+    'Violão, viola e ukulelé'
+)
+
+criaCartao(
+    'Pessoal',
+    'Qual minha série preferida?',
+    'Fearless 8 segundos para a glória'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+'Pessoal',
+'Qual a faculdade que pretendo fazer?',
+'Agronomia'
+
+
+criaCartao(
+'Pessoal',
+'Qual meu estilo de música preferido?',
+'Sertanejo'
+)
+
+criaCartao(
+'Pessoal',
+'Qual meu passa tempo que mais gosto?',
+'Ficar na fazenda'
+)
+
